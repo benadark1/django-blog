0456fd81141cbb6e5c8644848fdb008817f2c5aa
Benjamin Adarkwa <ben.2482000@yahoo.com>
2024-05-27 16:02:09 -0400

    Added Category Model- HW7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
 # a new admin registration
-admin.site.register(Post)
+# admin.site.register(Post)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)  # excluding post field
+
 # new admin registration for Category Model
-admin.site.register(Category)
+# admin.site.register(Category)
