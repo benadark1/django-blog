c59e4d116578ee472f593ab16e2f4eaeb5ea989a
Benjamin Adarkwa <ben.2482000@yahoo.com>
2024-06-03 01:43:31 -0400

    blogging app class-based views- HW8

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
 
 
 def stub_view(request, *args, **kwargs):
@@ -27,18 +40,18 @@ def old_list_view(request):
     return HttpResponse(body, content_type="text/html")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+# def list_view(request):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     posts = published.order_by('-published_date')
+#     context = {'posts': posts}
+#     return render(request, 'blogging/list.html', context)
+#
+#
+# def detail_view(request, post_id):
+#     published = Post.objects.exclude(published_date__exact=None)
+#     try:
+#         post = published.get(pk=post_id)
+#     except Post.DoesNotExist:
+#         raise Http404
+#     context = {'post': post}
+#     return render(request, 'blogging/detail.html', context)
