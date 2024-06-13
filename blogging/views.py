c8c6fde875ace32d029058bc4765c138c3a8a7b4
Benjamin Adarkwa <ben.2482000@yahoo.com>
2024-06-13 02:15:01 -0400

    API add on !

--- blogging/views.py
+++ blogging/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
-from blogging.models import Post
+from django.contrib.auth.models import User
+from blogging.models import Post, Category
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from rest_framework import permissions, viewsets
+
+from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
 
 
 class PostListView(ListView):
@@ -42,6 +46,35 @@ def old_list_view(request):
     body = template.render(context)
     return HttpResponse(body, content_type="text/html")
 
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
+    queryset = User.objects.all().order_by("-date_joined")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows post to be viewed or edited.
+    """
+
+    queryset = Post.objects.all().order_by("-published_date")
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows categories to be viewed or edited.
+    """
+
+    queryset = Category.objects.all().order_by("title")
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 # def list_view(request):
 #     published = Post.objects.exclude(published_date__exact=None)
